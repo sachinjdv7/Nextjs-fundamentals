22786384971646454227427e7bfc13a6e0567717
sachinjadhav <sachinjdv7@gmail.com>
2024-07-31 21:16:33 +0530

    static and dynamic rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -9,11 +9,15 @@ const UsersPage = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const users: User[] = await res.json();
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
   );
 };
 
