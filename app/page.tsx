492166e2aef3eff99b031c7239f78d6197e9070b
sachinjadhav <sachinjdv7@gmail.com>
2024-07-31 18:14:45 +0530

    basics of routing and navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <main>Hello world</main>;
+  return (
+    <main>
+      Hello world
+      <Link href="/users">users</Link>
+    </main>
+  );
 }
