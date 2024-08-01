c285d3ca72b9545984b23314e151e66353e40c70
sachinjadhav <sachinjdv7@gmail.com>
2024-08-01 20:03:43 +0530

    post api create

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -12,3 +12,12 @@ export function GET(request: NextRequest) {
     },
   ]);
 }
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  if (!body.name)
+    return NextResponse.json({ error: "name is required" }, { status: 400 });
+
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+}
