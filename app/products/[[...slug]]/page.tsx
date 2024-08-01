b87dcec63f2a1b2ddaf24df6dfa6d074699467c9
sachinjadhav <sachinjdv7@gmail.com>
2024-08-01 09:28:48 +0530

    nested mulitple product route

---
+++ app/products/[[...slug]]/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {
+  params: {
+    slug: string[];
+  };
+}
+
+const ProductDetails = ({ params: { slug } }: Props) => {
+  return <div>ProductDetails{slug}</div>;
+};
+
+export default ProductDetails;
