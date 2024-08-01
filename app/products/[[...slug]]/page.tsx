65f77baec43fead71e598a4a9e8ea1f43d822398
sachinjadhav <sachinjdv7@gmail.com>
2024-08-01 10:01:20 +0530

    sort table ascending order by name or email

--- app/products/[[...slug]]/page.tsx
+++ app/products/[[...slug]]/page.tsx
@@ -4,10 +4,20 @@ interface Props {
   params: {
     slug: string[];
   };
+  searchParams: {
+    sortOrder: string;
+  };
 }
 
-const ProductDetails = ({ params: { slug } }: Props) => {
-  return <div>ProductDetails{slug}</div>;
+const ProductDetails = ({
+  params: { slug },
+  searchParams: { sortOrder },
+}: Props) => {
+  return (
+    <div>
+      ProductDetails{slug},{sortOrder}
+    </div>
+  );
 };
 
 export default ProductDetails;
--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,22 +1,37 @@
+import Link from "next/link";
+import { sort } from "fast-sort";
+
 interface User {
   id: number;
   name: string;
   email: string;
 }
 
-const UserTable = async () => {
+interface Props {
+  sortOrder: string;
+}
+
+const UserTable = async ({ sortOrder }: Props) => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const users: User[] = await res.json();
+
+  const sortedUser = sort(users).asc(
+    sortOrder === "email" ? (user) => user.email : (user) => user.name
+  );
   return (
     <table className="table table-bordered">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Email</th>
+          <th>
+            <Link href="/users?sortOrder=name">Name</Link>
+          </th>
+          <th>
+            <Link href="/users?sortOrder=email">Email</Link>
+          </th>
         </tr>
       </thead>
       <tbody>
-        {users.map((user) => (
+        {sortedUser.map((user) => (
           <tr key={user.id}>
             <td>{user.name}</td>
             <td>{user.email}</td>
