ef89d23118afbc2df60e4b362a6428083d94734b
sachinjadhav <sachinjdv7@gmail.com>
2024-08-01 18:18:37 +0530

    not-found custom error messages

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return <div>Thre Requested page doesn't exists</div>;
+};
+
+export default NotFoundPage;
--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,9 +1,12 @@
+import { notFound } from "next/navigation";
+
 interface Props {
   params: {
     id: number;
   };
 }
 const UserDetailPage = ({ params: { id } }: Props) => {
+  if (id > 10) notFound();
   return <div>UserpageDetails{id}</div>;
 };
 
