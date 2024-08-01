f0f35a3ea64bbe0902f67082bd62ae524b44458f
sachinjadhav <sachinjdv7@gmail.com>
2024-08-01 09:20:19 +0530

    nested photo with id route

---
+++ app/users/[id]/photos/[photoId]/page.tsx
@@ -0,0 +1,17 @@
+interface Props {
+  params: {
+    id: number;
+    photoId: number;
+  };
+}
+
+const PhotoDetails = ({ params: { id, photoId } }: Props) => {
+  return (
+    <div>
+      PhotoDetails{id}
+      {photoId}
+    </div>
+  );
+};
+
+export default PhotoDetails;
