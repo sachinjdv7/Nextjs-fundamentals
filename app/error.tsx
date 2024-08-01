d7a6cdba02b72cccc56a2ab9977164eda0a8233e
sachinjadhav <sachinjdv7@gmail.com>
2024-08-01 19:00:53 +0530

    unexpected errors handled

---
+++ app/error.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+interface Props {
+  error: Error;
+  reset: () => void;
+}
+const ErrorPage = ({ error, reset }: Props) => {
+  return (
+    <>
+      <div>An unexpected error has occured</div>
+      <button className="btn" onClick={() => reset()}>
+        Retry
+      </button>
+    </>
+  );
+};
+
+export default ErrorPage;
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NotFoundPage = () => {
-  return <div>Thre Requested page doesn't exists</div>;
+  return <div>Thre Requested page doesn&apos;t exists</div>;
 };
 
 export default NotFoundPage;
