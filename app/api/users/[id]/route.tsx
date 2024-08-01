3949c709d74d54f305f0737e7f8a29f0d97c45b8
sachinjadhav <sachinjdv7@gmail.com>
2024-08-01 20:26:09 +0530

    put api create

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -13,3 +13,14 @@ export function GET(request: NextRequest, { params: { id } }: Props) {
 
   return NextResponse.json({ id: 1, name: "sachin" });
 }
+
+export async function PUT(request: NextRequest, { params: { id } }: Props) {
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+
+  if (id > 10)
+    return NextResponse.json({ error: "User  is not found" }, { status: 404 });
+
+  return NextResponse.json({ id: 1, name: body.name });
+}
