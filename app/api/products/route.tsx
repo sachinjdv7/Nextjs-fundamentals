1b6629102365d34b0ae319ce29db41e090bea517
sachinjadhav <sachinjdv7@gmail.com>
2024-08-02 22:26:25 +0530

    product get and post api created

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,11 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "milk", price: 45 },
-    { id: 2, name: "orange", price: 50 },
-  ]);
+export async function GET(request: NextRequest) {
+  const products = prisma.product.findMany();
+  return NextResponse.json(products);
 }
 
 export async function POST(request: NextRequest) {
@@ -16,5 +15,16 @@ export async function POST(request: NextRequest) {
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  return NextResponse.json({ id: 10, name: body.name, price: body.price });
+  // await prisma.product.findUnique({
+  //   where: { name: body.name }
+  // });
+
+  const newProduct = await prisma.product.create({
+    data: {
+      name: body.name,
+      price: body.price,
+    },
+  });
+
+  return NextResponse.json(newProduct, { status: 201 });
 }
