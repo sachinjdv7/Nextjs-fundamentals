06ae9a901c01ced82853cadfef9738e4495afa50
sachinjadhav <sachinjdv7@gmail.com>
2024-08-04 19:24:39 +0530

    email basic template setup

---
+++ emails/WelcomeTemplate.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  Html,
+  Body,
+  Container,
+  Text,
+  Link,
+  Preview,
+} from "@react-email/components";
+
+const WelcomeTemplate = ({ name }: { name: string }) => {
+  return (
+    <Html>
+      <Preview>Welcome abord!</Preview>
+      <Body>
+        <Container>
+          <Text>Hello {name}</Text>
+          <Link href="https://codewithmosh.com">www.codewithmosh.com</Link>
+        </Container>
+      </Body>
+    </Html>
+  );
+};
+
+export default WelcomeTemplate;
