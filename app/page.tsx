6b444aa4101764681b745944ab1abdc2ef0aa92d
sachinjadhav <sachinjdv7@gmail.com>
2024-08-05 14:46:35 +0530

    Lazy loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,17 @@
-import Link from "next/link";
-import ProductCart from "../app/components/ProductCart";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
-import Image from "next/image";
-import hanuman from "@/public/images/hanuman.jpg";
+"use client";
+import { useState } from "react";
+import dynamic from "next/dynamic";
+const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
+  loading: () => <p>Loading...</p>,
+});
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
+export default function Home() {
+  const [isVisible, setIsVisible] = useState(false);
   return (
-    <main className="relative h-screen">
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-      <Link href="/users">users</Link>
-      <ProductCart />
-      <Image
-        src="https://bit.ly/react-cover"
-        alt="lord hanuman"
-        fill
-        className="object-cover"
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        quality={100}
-        priority
-      />
-    </main>
+    <>
+      <main>Hello world</main>
+      <button onClick={() => setIsVisible(true)}>Show</button>
+      {isVisible && <HeavyComponent />}
+    </>
   );
 }
